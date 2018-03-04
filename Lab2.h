34ad0656f78d9f1a27645908f598e3cb0178792e
Leantha Naicker <788753@students.wits.ac.za>
2018-03-04 15:16:15 +0200

    submission 1

---
+++ Lab2.h
@@ -0,0 +1,6 @@
+#ifndef LAB2_H
+#define LAB2_H
+
+void swap(int *elem1, int *elem2);
+
+#endif 
---
+++ Lab_2.c
@@ -0,0 +1,9 @@
+#include "Lab2.h"
+
+void swap(int *elem1, int *elem2)
+{
+    int temp = *elem1;
+    *elem1 = *elem2;
+    *elem2 = temp;
+}
+
